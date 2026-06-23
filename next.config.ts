0ce890afd430c342d5fdadfbd3485410ebb35869
L1ghtn1ng <jay@cybermon.uk>
2026-06-23 19:48:13 +0100

    Remove un-needed security header as its covered by the csp

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 const securityHeaders = [
   { key: 'X-DNS-Prefetch-Control', value: 'on' },
-  { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
   { key: 'X-Content-Type-Options', value: 'nosniff' },
   { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
   { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=()' },
